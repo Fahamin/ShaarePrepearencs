cbf25d29b266e74531328fb0d1e0a78c8f280f10
Fahamin <fahamin00@gmail.com>
2019-01-18 10:08:03 -0800

    activity change to use sp

--- app/src/main/java/coin/bit/shaareprepearencs/MainActivity.java
+++ app/src/main/java/coin/bit/shaareprepearencs/MainActivity.java
@@ -1,49 +1,53 @@
 package coin.bit.shaareprepearencs;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-EditText etName,etEmail;
-Button saveBtn,retriveBtn,clearBtn;
+    EditText etName, etEmail;
+    Button saveBtn, retriveBtn, clearBtn;
 
     SharedPreferences sharedPreferences;
-    public static final String name="namekey";
+    public static final String name = "namekey";
     public static final String email = "emailkey";
-    public static final String file= "myfile";
-
+    public static final String file = "myfile";
+    CheckBox checkBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etEmail=findViewById(R.id.editEmail);
-        etName=findViewById(R.id.editName);
+        //checkBox = findViewById(R.id.checkboxID);
+        etEmail = findViewById(R.id.editEmail);
+        etName = findViewById(R.id.editName);
 
-        saveBtn=findViewById(R.id.BtnSave);
-        clearBtn=findViewById(R.id.BtnClear);
-        retriveBtn=findViewById(R.id.BtnRetrive);
+        //  int a =  checkBox.setChecked(true);
+
+        saveBtn = findViewById(R.id.BtnSave);
+        clearBtn = findViewById(R.id.BtnClear);
+        retriveBtn = findViewById(R.id.BtnRetrive);
 
         sharedPreferences = getSharedPreferences(file, Context.MODE_PRIVATE);
 
-        if(sharedPreferences.contains(name))
+        if (sharedPreferences.contains(name)) {
+            etName.setText(sharedPreferences.getString(name, ""));
+        }
+        else
         {
-            etName.setText(sharedPreferences.getString(name,""));
+            startActivity(new Intent(this,login.class));
         }
 
-        if(sharedPreferences.contains(email))
-        {
-            etEmail.setText(sharedPreferences.getString(email,""));
+        if (sharedPreferences.contains(email)) {
+            etEmail.setText(sharedPreferences.getString(email, ""));
         }
-
-
-
     }
 
     public void save(View view) {
@@ -52,26 +56,24 @@ Button saveBtn,retriveBtn,clearBtn;
 
         SharedPreferences.Editor editor = sharedPreferences.edit();
 
-        editor.putString(name,getName);
-        editor.putString(email,getEmail);
+        editor.putString(name, getName);
+        editor.putString(email, getEmail);
         editor.commit();
     }
 
     public void Get(View view) {
-        if(sharedPreferences.contains(name))
-        {
-            etName.setText(sharedPreferences.getString(name,""));
+        if (sharedPreferences.contains(name)) {
+            etName.setText(sharedPreferences.getString(name, ""));
         }
-        if(sharedPreferences.contains(email))
-        {
-            etEmail.setText(sharedPreferences.getString(email,""));
+        if (sharedPreferences.contains(email)) {
+            etEmail.setText(sharedPreferences.getString(email, ""));
         }
 
     }
 
     public void clear(View view) {
-
-        etEmail.setText("");
-        etName.setText("");
+sharedPreferences.edit().clear().commit();
+       /* etEmail.setText("");
+        etName.setText("");*/
     }
 }
---
+++ app/src/main/java/coin/bit/shaareprepearencs/login.java
@@ -0,0 +1,13 @@
+package coin.bit.shaareprepearencs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class login extends AppCompatActivity {
+    //sharedPreferences.edit().remove(name).commit();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
